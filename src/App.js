4bf80a1b185ba9aedf4c2df2af718c228aba3249
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-02-27 18:51:40 -0500

    update

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class Counter extends Component {
     height: .5,
     width: 230,
     borderColor : 'red'
-}}/> First React Project | Copyright @ MH Jafory</h1>
+}}/> First React Project | Copyright @ <a herf="http://mhjaofry.com"> MH Jafory </a></h1>
       </div>
 
 
@@ -114,82 +114,3 @@ class Counter extends Component {
   }
 }
 export default Counter;
-
-
-
-
-
-
-
-// import React, {Component} from 'react'
-
-//
-// class Test extends Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//
-//             count: 0,
-//         }
-//     }
-//
-//
-//
-//     //this is where we are going to put your methods yay!
-//
-//     Increment = () => {
-//         this.setState({
-//             count: this.state.count + 1
-//         })
-//     }
-//
-//     Decrement = () => {
-//         this.setState({
-//             count: this.state.count - 1
-//         })
-//     }
-//
-//     render() {
-//         return (
-//
-//             <div className="Containner">
-//                 <nav className"navbar">Counter</nav>
-//                 <div className"counter">
-//                   <h1>{this.state.count}</h1>
-//                   <button className="button" onClick = {this.Increment}> Increment </button>
-//                   <button className="button" onClick={this.Decrement}> Decrement </button>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-//
-// export default Test;
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
