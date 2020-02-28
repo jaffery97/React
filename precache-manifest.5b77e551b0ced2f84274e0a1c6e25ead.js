54b29684d791f4eced187261a81f75d702507b26
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-02-27 19:03:59 -0500

    Updates

---
+++ precache-manifest.5b77e551b0ced2f84274e0a1c6e25ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec78b8e9815695d7476d516d82adca8",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "b1204e3850bef252f738",
+    "url": "/React/static/css/main.fe5954f3.chunk.css"
+  },
+  {
+    "revision": "18b76d096294d32f1f3f",
+    "url": "/React/static/js/2.f3d7838c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React/static/js/2.f3d7838c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1204e3850bef252f738",
+    "url": "/React/static/js/main.59eeb452.chunk.js"
+  },
+  {
+    "revision": "67521e6208942c5d3ec6",
+    "url": "/React/static/js/runtime-main.5322444d.js"
+  }
+]);
